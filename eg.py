fe9df64e296406cec5e29e063290533c2ecaa59c
QihanW <32336815+QihanW@users.noreply.github.com>
2022-04-19 15:00:25 -0400

    Update eg.py

--- eg.py
+++ eg.py
@@ -1,39 +1,78 @@
 import os
-
+ 
 import torch
 import torch.distributed as dist
 import torch.multiprocessing as mp
-#import torch.distributed.algorithms.model_averaging.averagers as averagers
 import torch.nn as nn
 import torch.optim as optim
-#import torch.distributed.algorithms.ddp_comm_hooks.post_localSGD_hook as post_localSGD
+import swarm_hook
+import gossip_hook
+import random
+import torch.distributed.algorithms.ddp_comm_hooks.default_hooks as comhook
+import torch.distributed.algorithms.ddp_comm_hooks.post_localSGD_hook as post_localSGD
+
 
 def run_worker(rank, world_size):
-    #torch.cuda.set_device(rank)
-    dist.init_process_group("nccl", rank=rank, world_size=world_size)
-    #dist.init_process_group(backend="gloo", init_method='env://')
-    torch.cuda.set_device(rank)
-    module = nn.Linear(1, 1, bias=False).cuda()
-    model = nn.parallel.DistributedDataParallel(
-        module, device_ids=[rank], output_device=rank
-    )
+   # Initialize the distributed environment.
+   torch.cuda.set_device(rank)
+   dist.init_process_group("nccl", rank=rank, world_size=world_size)
+ 
+   # Define the model architecture and wrap it up with DDP.
+   model = (
+       nn.Sequential(
+           nn.Conv2d(3, 64, kernel_size=3, padding=1),
+           nn.ReLU(),
+           nn.Conv2d(64, 2, kernel_size=3, padding=1),
+       )
+       .half()
+       .cuda()
+   )
+   ddp_model = nn.parallel.DistributedDataParallel(
+       module=model,
+       device_ids=[rank],
+   )
 
-    optimizer = optim.SGD(model.parameters(), lr=0.001)
-    loss_fn = nn.MSELoss()
+   samples = [i for i in range(world_size)]
+   mypeer = random.choice(samples)
+   if mypeer == rank:
+       mypeer = (rank + 1) % world_size
+   sub_group = dist.new_group([1,2])
+   mypeer = random.choice(samples)
+   
+ 
+   # Define loss function and optimizer.
+   loss_fn = nn.MSELoss()
+   optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
+   input = torch.randn(4, 3, 128, 128).half()
+   labels = torch.randn(4, 2, 128, 128).half().to(rank)
 
-    #averager = averagers.PeriodicModelAverager(period=4, warmup_steps=5)
-    for step in range(0, 10):
-        optimizer.zero_grad()
-        loss = loss_fn(output, labels)
-        loss.backward()
-        optimizer.step()
-        #averager.average_parameters(model.parameters())
-        print("Step {}: loss = {}".format(step, loss.item()))
-    # Cleanup.
-    dist.destroy_process_group()
+   # Register a post-localSGD communication hook.
+   #state = post_localSGD.PostLocalSGDState(process_group=None, subgroup=None, start_localSGD_iter=5)
+   #ddp_model.register_comm_hook(state, post_localSGD.post_localSGD_hook)
 
+   #state = swarm_hook.SwarmSGDState(process_group=None, subgroup=None, selected_workers=None)
+   #ddp_model.register_comm_hook(state, swarm_hook.swarm_SGD_hook)
+   
+   state = gossip_hook.GossipSGDState(process_group=None, subgroup=None, selected_workers=None)
+   ddp_model.register_comm_hook(state, gossip_hook.gossip_SGD_hook)
+
+
+   for step in range(0, 50):
+       optimizer.zero_grad()
+       output = ddp_model(input)
+       loss = loss_fn(output, labels).half()
+       loss.backward()
+       if step % 10 == 0 and rank == 0:
+           print("Step {}: loss = {}".format(step, loss.item()))
+       optimizer.step()
+   # Cleanup.
+   dist.destroy_process_group()
+
+
+ 
 if __name__ == "__main__":
    os.environ["MASTER_ADDR"] = "localhost"
    os.environ["MASTER_PORT"] = "12345"
    world_size = torch.cuda.device_count()
    mp.spawn(run_worker, args=(world_size,), nprocs=world_size, join=True)
+
